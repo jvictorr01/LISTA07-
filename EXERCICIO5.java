3a5edf9654cd6dea3bc66bcd6a5697f8d655894d
Joao Victor <103452608+jvictorr01@users.noreply.github.com>
2022-09-13 15:58:45 -0300

    Update EXERCICIO5.java

--- EXERCICIO5.java
+++ EXERCICIO5.java
@@ -1,10 +1,30 @@
-public class EXERCICIO5 {
-    //Escreva um programa que leia um número inteiro positivo  ́ n e em seguida imprima n linhas
-    //do chamado Triangulo de Pascal:
+import java.util.Scanner;
+
+public class EX5 {
 
     public static void main(String[] args) {
+        int n, l, valor, c;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Numero inteiro positivo:");
+        n = s.nextInt();
 
+        for (l = 1; l <= n; l++) {
+            valor = 1;
+            System.out.print(valor);
+            System.out.println("  ");
 
+            if (n != 1) {
+                for (l = 1; l < n; l++) {
+                    valor = 1;
+                    System.out.print(valor + " ");
 
+                    for (c = 1; c <= l; c++) {
+                        valor = (valor * (l - c + 1)) / c;
+                        System.out.print(valor + " ");
+                    }
+                    System.out.println("  ");
+                }
+            }
+        }
     }
 }
